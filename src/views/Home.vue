9f2507a8218623b75281aafc079c644e5218708f
StephaneMercier <stefane.mercier@gmail.com>
2022-04-14 17:18:22 +0200

    App done, CSS to do

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,78 @@
 <template>
   <h1>To Do App</h1>
-  <form>
+  <form @submit.prevent="addNewTodo">
     <label class="todoName" for="newTodo">Nouvelle Tâche</label>
-    <input name="newTodo" />
-    <button>Ajouter cette tâche</button>
+    <input v-model="newTodo" name="newTodo" />
+    <button>Ajouter</button>
   </form>
+  <button @click="removeAll">Effacer tout</button>
+  <button @click="markAllDone">Marquer tous comme "Fait"</button>
+  <div v-for="(todo, index) in todos" :key="todo.id">
+    <ul>
+      <li class="todo">
+        <h3 :class="{ done: todo.done }" @click="toggleDone(todo)">
+          {{ todo.content }}
+        </h3>
+        <button @click="removeTodo(index)">Effacer la tâche</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { ref } from "vue";
+export default {
+  setup() {
+    const newTodo = ref("");
+    const todos = ref([]);
+    function addNewTodo() {
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+      newTodo.value = "";
+    }
+    function toggleDone(todo) {
+      todo.done = !todo.done;
+    }
 
-export default {};
+    function removeTodo(index) {
+      todos.value.splice(index, 1);
+    }
+
+    function markAllDone() {
+      todos.value.forEach((todo) => (todo.done = true));
+    }
+
+    function removeAll() {
+      todos.value = [];
+    }
+
+    return {
+      todos,
+      newTodo,
+      addNewTodo,
+      toggleDone,
+      removeTodo,
+      markAllDone,
+      removeAll,
+    };
+  },
+};
 </script>
 
 <style scoped>
 h1 {
   text-decoration: underline;
 }
-.todoName {
+ul {
+  list-style-type: none;
+}
+.todo {
+  cursor: pointer;
+}
+.done {
+  text-decoration: line-through;
 }
 </style>
